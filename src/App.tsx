ecbc98b32331aa3ff57e10587df8c245d9e89004
Silviu <183800984+silviu28@users.noreply.github.com>
2025-05-24 11:46:44 +0300

    Fix image pathing

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <LanguageStats />
         <div className="grid-panel" id="main-view">
             <ProjectContainer>
-                <img src={"src/assets/restaurant2.png"}/>
+                <img src={"public/restaurant2.png"}/>
                 <h1>Restaurant Catalog</h1>
                 <p>A Windows app to see menus and items of a restaurant and place orders. Features an employee
                     control panel for users with required privileges.</p>
@@ -18,15 +18,15 @@ function App() {
             </ProjectContainer>
 
             <ProjectContainer>
-                <img src={"src/assets/shooter1.png"}/>
+                <img src={"public/shooter1.png"}/>
                 <h1>Top-down Shooter</h1>
-                <p>A native game based on NES' Battle City. Features a session browser to join multiplayer and a
+                <p>A native game based on NES' Battle City. Features a session browser to join multiplayer and
                     leaderboard for progression.</p>
                 <h2>C++, Qt, SQLite, HTTP</h2>
             </ProjectContainer>
 
             <ProjectContainer>
-                <img src={"src/assets/calc.png"}/>
+                <img src={"public/calc.png"}/>
                 <h1>Programmer Calculator</h1>
                 <p>An advanced calculator app that features decimal computations for bases 2-16 with 8-digit
                     precision.</p>
@@ -34,14 +34,14 @@ function App() {
             </ProjectContainer>
 
             <ProjectContainer>
-                <img src={"src/assets/cards.png"}/>
+                <img src={"public/cards.png"}/>
                 <h1>Card Matching Game</h1>
                 <p>Extensible card matching game with the ability to save prior game session.</p>
                 <h2>.NET, C#, WPF, JSON</h2>
             </ProjectContainer>
 
             <ProjectContainer>
-                <img src={"src/assets/controller.png"}/>
+                <img src={"public/controller.png"}/>
                 <h1>Companion App</h1>
                 <p>An Android app to remotely control an Arduino miniature car via Bluetooth.</p>
                 <h2>Android, Java, Bluetooth, Arduino</h2>
